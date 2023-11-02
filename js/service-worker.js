6766b53ce4687a357346636c38c509bfd16de9d3
“emutara” <emu3759@yahoo.co.jp>
2023-11-02 17:03:30 +0900

    pwa対応

---
+++ js/service-worker.js
@@ -0,0 +1,47 @@
+// キャッシュしたいファイルの一覧を指定 --- (*1)
+const cacheName = 'v1';
+var urlsToCache = [
+  // '/',
+  '/index.html',
+  '/manifest.json',
+  '/js/service-worker.js',
+];
+
+// インストール時に実行されるイベント --- (*2)
+self.addEventListener('install', event => {
+  //waitUntil waitUnitl 内に記述されたコードが成功しない限り install が完了しないことを保証してくれます。
+  event.waitUntil(
+    // キャッシュしたいファイルを指定
+    caches.open(cacheName).then(cache => {
+      console.log("[Service Worker] Caching all: app shell and content");
+      return cache.addAll(urlsToCache)//cache.addAll cache.open で指定したキャッシュ空間に対象となるファイルを保存する関数です。
+    })
+  )
+});
+
+// インストール後に実行されるイベント
+self.addEventListener('activate', e => {
+  // 必要に応じて古いキャッシュの削除処理などを行う
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches
+          .match(event.request)
+          .then(function(response) {
+              return response ? response : fetch(event.request);
+          })
+  );
+});
